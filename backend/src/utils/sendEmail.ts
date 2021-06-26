10ee0871c9db8300bed8b6979199e64b1bf01a4c
Shobhit Yadav <shobityadav23@gmail.com>
2021-06-25 22:08:36 -0700

    Added Env Variables ans Fixed Some Bugs

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -1,14 +1,17 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
 
 export async function sendEmail(email: string, url: string) {
+
+  dotenv.config()
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: 'shobityadav23@gmail.com', // generated ethereal user
-      pass: 'attackOnDemons23bond1@' // generated ethereal password
+      user: process.env.USER_MAIL, // generated ethereal user
+      pass: process.env.USER_PASSWORD // generated ethereal password
     }
   });
 
